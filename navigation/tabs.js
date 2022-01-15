b90e1ca5c33cc31265a41c54c36f5537baf3c670
toannguyen2603 <huutoan2603@gmail.com>
2022-01-15 23:43:09 +0700

    create navigation custom

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,20 +1,104 @@
 import React from 'react';
 
-import {View, Image, TouchableOpacity} from 'react-native';
+import {View, Image, Platform, TouchableOpacity} from 'react-native';
+import {icons, COLORS} from '../constants';
 
 import {
   createBottomTabNavigator,
   BottomTabBar,
 } from '@react-navigation/bottom-tabs';
+import Svg, {Path} from 'react-native-svg';
 
 import {Home} from '../src/screens';
-import {COLORS, icons} from '../constants';
 
 const Tab = createBottomTabNavigator();
 
+const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
+  var isSelected = accessibilityState.selected;
+
+  if (isSelected) {
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flexDirection: 'row', position: 'absolute', top: 0}}>
+          <View style={{flex: 1, backgroundColor: COLORS.white}}></View>
+          <Svg width={70} height={61} viewBox="0 0 75 61">
+            <Path
+              d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+              fill={COLORS.white}
+            />
+          </Svg>
+          <View style={{flex: 1, backgroundColor: COLORS.white}}></View>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            top: -22.5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: COLORS.white,
+          }}
+          onPress={onPress}>
+          {children}
+        </TouchableOpacity>
+      </View>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          height: 60,
+          backgroundColor: COLORS.white,
+        }}
+        activeOpacity={1}
+        onPress={onPress}>
+        {children}
+      </TouchableOpacity>
+    );
+  }
+};
+
+const CustomTabBar = props => {
+  if (Platform == 'ios') {
+    return (
+      <View>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 30,
+            backgroundColor: COLORS.white,
+          }}></View>
+        <BottomTabBar {...props.props} />
+      </View>
+    );
+  } else {
+    return <BottomTabBar {...props.props} />;
+  }
+};
+
 const Tabs = () => {
   return (
-    <Tab.Navigator tabBarOptions={{showLabel: false}}>
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          position: 'absolute',
+          left: 0,
+          bottom: 0,
+          right: 0,
+          borderTopWidth: 0,
+          backgroundColor: 'transparent',
+          elevation: 0,
+        },
+      }}
+      tabBar={props => <CustomTabBar props={props} />}>
+      {/* Tab home */}
       <Tab.Screen
         name="Home"
         component={Home}
@@ -30,9 +114,11 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
 
+      {/* Tab search */}
       <Tab.Screen
         name="Search"
         component={Home}
@@ -48,8 +134,11 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
+
+      {/* Tab Like */}
       <Tab.Screen
         name="Like"
         component={Home}
@@ -65,8 +154,11 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
+
+      {/* Tab User */}
       <Tab.Screen
         name="User"
         component={Home}
@@ -82,6 +174,7 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
     </Tab.Navigator>
